a0df56865fc2b60e7f36d75de6701a0635965479
Sip - userbot <110762610+sip-userbot@users.noreply.github.com>
2022-09-22 00:41:43 +0700

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,40 @@
+import os
+import logging
+from pyromod import listen
+from pyrogram import Client, idle
+from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
+
+
+logging.basicConfig(
+    level=logging.WARNING, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+API_ID = int(os.environ.get('API_ID', 0))
+API_HASH = os.environ.get('API_HASH', None)
+BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
+
+app = Client(
+    ":memory:",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="Heroku"),
+)
+
+
+
+
+
+if __name__ == "__main__":
+    try:
+        app.start()
+    except (ApiIdInvalid, ApiIdPublishedFlood):
+        raise Exception("Your API_ID/API_HASH salah ngentot.")
+    except AccessTokenInvalid:
+        raise Exception("Your BOT_TOKEN Salah anjir")
+    uname = app.get_me().username
+    print(f"@{uname} Berhasil")
+    idle()
+    app.stop()
+    print("Bot mati.")
