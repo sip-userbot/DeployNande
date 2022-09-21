7ab8e27b6b9c90478b00ef6d2c2965a5a3c5310d
Sip - userbot <110762610+sip-userbot@users.noreply.github.com>
2022-09-22 00:09:49 +0700

    Add files via upload

---
+++ text.py
@@ -0,0 +1,24 @@
+from pyrogram.types import InlineKeyboardButton
+
+
+
+class Data:
+    # Start Message
+
+    START = """
+Halo 🙋 {}
+Saya adalah {}
+Bot ini berfungsi untuk menjalankan Prime Userbot Diheroku.
+Dengan Mudah\n
+#Wajib
+❗INFO MEMBUATNYA MEMBUTUHKAN AKUN HEROKU\n
+Terimakasih ...
+Maintened bot by @PandaUserbot
+    """
+
+
+    # 
+    buttons = [     
+        [InlineKeyboardButton("Prime-Userbot", callback_data="prime")],]
+   
+    
