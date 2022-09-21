634023a02ca9a935fed1f8e5577e531ee53bc635
Sip - userbot <110762610+sip-userbot@users.noreply.github.com>
2022-09-22 00:33:43 +0700

    Add files via upload

---
+++ start.py
@@ -0,0 +1,16 @@
+from text import Data
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+
+# Start Message
+@Client.on_message(filters.private & filters.incoming & filters.command("start"))
+async def start(bot, msg):
+	user = await bot.get_me()
+	mention = user["mention"]
+	await bot.send_message(
+		msg.chat.id,
+		Data.START.format(msg.from_user.mention, mention),
+		reply_markup=InlineKeyboardMarkup(Data.buttons)
+	)
